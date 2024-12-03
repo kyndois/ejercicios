f339875ae55611349258d7f41f94da1632a56257
kyndois <carlosfcolopezc@gmail.com>
2024-12-03 11:30:13 +0100

    zorro

---
+++ src/bucleMultiplicacionRusa.kt
@@ -0,0 +1,24 @@
+// PREGUNTA 17 - K FUNCIONES SENCILLAS
+
+fun main() {
+    println(mulRusa(105,1))
+    println(mulRusa(1,105))
+    println(mulRusa(1999,33))
+    println(mulRusa(210,10000))
+    println(mulRusa(105,68))
+}
+
+fun mulRusa(num1: Int, num2: Int): Int {
+    if(num2 == 1) return num1           // Si el segundo número es 1, será la única coincidencia
+    var result = 0                      // Variable para gestionar rel resultado e ir sumando
+    var newNum1 = num1                  // Recogerá el Número 1 e irá cambiado con la multiplicación
+    var newNum2 = num2                  // Recogerá el Número 2 e irá cambiado con la división
+    while (newNum2 != 0){               // Mientras el resto no sea 0 durará el bucle
+        if (newNum2 % 2 != 0){          // Si [newNum2] es impar
+            result += newNum1           // Suma [newNum1] al resultado
+        }
+        newNum1 *= 2                    // Se multiplica [newNum1] * 2
+        newNum2 /= 2                    // Se divide [newNum2] / 2
+    }
+    return result                       // Se devuelve el resultado
+}
---
+++ src/veniVidiVici.kt
@@ -0,0 +1,27 @@
+// PREGUNTA 20 - K FUNCIONES SENCILLAS
+
+fun main() {
+    println(cesar("Alea iacta est", 3))
+    println(cesar("Ave, Caesar, morituri te salutant", 7))
+    println(cesar("VW VCODKGP, DTWVQ?", -2))
+    println(cesar("a",-1))
+    println(cesar("a",-26))
+}
+
+fun cesar(txt: String, crypto: Int): String {
+    var decrypt = ""
+    val alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for (l in txt) {                                                 // Recorremos el texto
+        if (l.isLetter()) {                                          // Check si es una letra o no
+            val letter = alfabeto.indexOf(l.uppercaseChar())         // Localizamos la posición de la letra en nuestro alfabeto convirtiendo en mayusculas
+            var newLetter =(letter + crypto) % 26                    // Creamos la posición de la nueva letra y nos aseguramos que esté
+            if (newLetter < 0) {                                     // Check si el resultado es negativo para empezar desde el final
+                newLetter += 26                                      // Volvemos a recuperar el valor dentro de los 26 positivos
+            }
+            decrypt += alfabeto[newLetter]                           // Agregamos a nuestro resultado la letra en la posición obtenida
+        } else {
+            decrypt += l                                             // Agregamos el caracter sin modificar al resultado
+        }
+    }
+    return decrypt
+}
