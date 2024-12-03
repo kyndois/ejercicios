f293ed170c53f264aa53fa2fd4c52a4b21e297e6
kyndois <carlosfcolopezc@gmail.com>
2024-12-03 11:42:28 +0100

    multiplicacion rusa recursiva

---
+++ src/recursiveMulRusa.kt
@@ -0,0 +1,21 @@
+fun main(){
+    println(mulRusa2(105,1))
+    result = 0
+    println(mulRusa2(1,105))
+    result = 0
+    println(mulRusa2(1999,33))
+    result = 0
+    println(mulRusa2(210,10000))
+    result = 0
+    println(mulRusa2(105,68))
+}
+var result = 0
+fun mulRusa2(num1: Int, num2: Int):Int{
+    if(num2 % 2 != 0){                          // Si [num2] es impar
+        result += num1                          // Se suma [num1] a la variable global
+    }
+    if(num2/2 != 0){                            // Si [num2] es divisible entre 2
+        mulRusa2(num1*2, num2/2)    // Se vuelve a llamar a la función multiplicando [num1]*2 y dividiendo [num2]/2
+    }
+    return result                               // Se devuelve el resultado
+}
