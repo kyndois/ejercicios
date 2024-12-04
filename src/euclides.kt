7980563458b3a70d5faa9c2ef015f66b907f5936
Fran <carlosfcolopezc@gmail.com>
2024-12-04 12:04:30 +0100

    una de gatos y ratones

---
+++ src/euclides.kt
@@ -0,0 +1,7 @@
+fun main(){
+    println(""+mcd(20,10)+", "+mcd(20,8)+", "+mcd(20,6)+", "+mcd(72,16))
+}
+fun mcd(num1: Int, num2: Int):Int {
+    if(num1%num2 == 0) return num2              // Si el [num2] es el mcd
+    return mcd(num2, num1%num2)           // Vuelve a llamar a la función, esta vez con [num2] como el resto obtenido
+}
---
+++ src/gatosYratones.kt
@@ -0,0 +1,41 @@
+/*Entrada: Una serie de casos. Cada caso es una única línea que contiene cuatro enteros que indican el
+número de plantas,
+número de habitaciones por planta y
+la planta y habitación donde se encuentra la rata.  Las plantas y habitaciones comienzan a numerarse desde 1.
+
+Salida: para cada caso, plano de la casa en la que se observa como los gatos rodean a la rata en las celdas adyacentes.
+Ten encuentra que puede haber hasta 8 celdas adyacentes y que solo se representan los gatos que están en las habitaciones adyacentes.
+Cada plano se separa del siguiente por una línea en blanco.  Cada habitación se imprime entre []*/
+
+fun main(){
+    var attempt = readlnOrNull()
+    while (attempt!=null){
+        val listChar = attempt.split(" ")
+        gyr(listChar[0].toInt(),listChar[1].toInt(),listChar[2].toInt(),listChar[3].toInt())
+        attempt = readlnOrNull()
+    }
+}
+
+fun gyr(flats: Int, rooms: Int, ratFlat: Int, ratRoom: Int){
+    var house = mutableListOf<List<String>>()
+    for(f in 1..flats){
+        var roomsState = mutableListOf<String>()
+        for(r in 1 .. rooms){
+            if(f == ratFlat && r == ratRoom){
+                roomsState.add("R")
+            }else if (ratFlat-f !in -1 .. 1 || ratRoom-r !in -1 .. 1){
+                roomsState.add("X")
+            }else{
+                roomsState.add("G")
+            }
+        }
+        house.add(roomsState.toList())
+    }
+
+    for (f in house){
+        for (i in f){
+            print("[$i]")
+        }
+        println()
+    }
+}
