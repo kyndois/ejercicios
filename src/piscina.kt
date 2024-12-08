8dc73c30875d3344c43f084a20e68786913a5eba
kyndois <kyndois@hotmail.com>
2024-12-08 19:31:46 +0100

    Piscina

--- src/piscina.kt
+++ src/piscina.kt
@@ -1,60 +1,38 @@
 fun main() {
-//    val size = readln()
-//    val pool = mutableListOf<CharArray>()
-//    for(i in 0 ..< size[0].digitToInt()){
-//        val newRow = readln()
-//        val row = CharArray(size[size.length-1].digitToInt())
-//        for(f in row.indices){
-//            row[f] = newRow[f]
-//        }
-//        pool.add(row)
-//    }
-//    val dropBall = readln()
-
-    val size = "5 5"
-    val pool = listOf(
-        charArrayOf('A', 'A', 'A', 'A', 'A'),
-        charArrayOf('A', 'A', 'A', 'T', 'A'),
-        charArrayOf('T', 'T', 'T', 'T', 'T'),
-        charArrayOf('A', 'T', 'A', 'A', 'A'),
-        charArrayOf('A', 'T', 'A', 'A', 'A')
-    )
-    val dropRow = 4
-    val dropCol = 4
-
-    fun mismaPiscina(row: Int, col: Int):Boolean{
-        for (i in row .. dropRow){
-            if(pool[i][col]=='T') return false
-            for(j in col .. dropCol){
-                if(pool[i][j]=='T') return false
+    var size = readln()
+    while(size != "0 0") {
+        val pool = mutableListOf<CharArray>()
+        repeat (size[0].digitToInt()) {
+            val newRow = readln()
+            val row = CharArray(size[size.length - 1].digitToInt())
+            for (f in row.indices) {
+                row[f] = newRow[f]
             }
+            pool.add(row)
         }
-        for (i in row downTo dropRow){
-            if(pool[i][col]=='T') return false
-            for(j in col .. dropCol){
-                if(pool[i][j]=='T') return false
-            }
-        }
+        val dropBall = readln()
 
-        return true
-    }
-
-    fun contaminar(row: Int) {
-        if (row !in pool.indices) return
-        for (col in 0..<pool[0].size){
-            if(mismaPiscina(row, col)){
+        fun contaminar(row: Int, col: Int) {
+            if (row !in pool.indices) return
+            if (col !in 0..pool[0].size-1) return
+            if (pool[row][col] != 'T' && pool[row][col] != 'R') {
                 pool[row][col] = 'R'
-
+                contaminar(row + 1, col)
+                contaminar(row - 1, col)
+                contaminar(row, col + 1)
+                contaminar(row, col - 1)
             }
-            contaminar(row+1)
-        }
-    }
 
-    contaminar(0)
-    for (p in pool) {
-        for (letter in p) {
-            print(letter)
         }
-        println()
+
+        contaminar(dropBall[0].digitToInt(), dropBall[dropBall.lastIndex].digitToInt())
+        for (p in pool) {
+            for (letter in p) {
+                print(letter)
+            }
+            println()
+        }
+        println("---------------------------------------------")
+        size = readln()
     }
 }
