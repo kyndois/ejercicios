7e4ca0cd0edc34a60aa7b67a45672a5966dbfe2e
kyndois <carlosfcolopezc@gmail.com>
2025-01-09 10:39:46 +0100

    flavio

---
+++ src/flavio.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var entrada = readln()
+    while(entrada != "0 0"){
+        val soldiers = mutableListOf<Int>()
+        for (i in 1..entrada.split(" ")[0].toInt()){
+            soldiers.add(i)
+        }
+        val jumpSolider= entrada.split(" ")[1].toInt()
+        var actualSoldier = 0
+        while (soldiers.size != 1){
+            actualSoldier = (actualSoldier+jumpSolider)%soldiers.size
+            soldiers.removeAt(actualSoldier)
+        }
+        println(soldiers[0])
+        entrada = readln()
+    }
+}
